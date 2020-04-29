96f42d2dd4c4b4b1d17472e582df3485b2ec273e
Ruslan Sakevych <xlionell@gmail.com>
2020-04-28 21:27:35 -0700

    Update PARCS python

--- python/echo/main.py
+++ python/echo/main.py
@@ -2,7 +2,6 @@ from parcs.server import Service, serve
 
 class Echo(Service):
     def run(self):
-        msg = self.recv()
-        self.send(msg)
+        self.send(self.recv())
 
 serve(Echo())
